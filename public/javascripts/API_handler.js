857dfed415d04c790c29f1da8594dca8e9024195
mashit <hi_2smart@yahoo.co.in>
2013-07-15 04:57:40 +0530

    feb

--- public/javascripts/API_handler.js
+++ public/javascripts/API_handler.js
@@ -29,3 +29,58 @@ var travel_handler = (function(){
 })();
 
 
+
+
+
+function parseParams(str) {
+    var obj = {}, pair;
+    var pairs = decodeURIComponent(str).split( "&" );
+    var injectParam = function(key, val) {
+        var firstBracket = key.indexOf('[');
+ 
+        if (firstBracket === -1) {
+            obj[key] = val;
+            return;
+        }
+ 
+        var prevkey = key.substring(0, firstBracket),
+            key = key.substr(firstBracket),
+            prev = obj,
+            newobj,
+            newkey;
+ 
+        key.replace(/\[([^\]]+)?\]/g, function(chunk, idx, pos) {
+            var newobj, newkey;
+            if (chunk.match(/\[\d*\]/)) {
+                newobj = prev[prevkey] || [];
+                newkey = idx || '[]';
+            } else {
+                newobj = prev[prevkey] || {};
+                newkey = idx;
+            }
+ 
+            if (prevkey === '[]') {
+                prev.push(newobj);
+            } else {
+                prev[prevkey] = newobj;
+            }
+ 
+            prev = newobj;
+            prevkey = newkey;
+        });
+ 
+        if (prevkey === '[]') {
+            prev.push(val);
+        } else {
+            prev[prevkey] = val;
+        }
+    }
+    
+ 
+    for( var arg = 0; arg < pairs.length; arg++ ) {
+        pair = pairs[ arg ].split( "=" );
+        injectParam(pair[0], pair[1]);
+    }
+ 
+    return obj;
+}
